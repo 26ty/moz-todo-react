c7dc67a7a4a264289a028c7a1e6c7f0905f3a506
“26tygit config --global user.name “ch1342213422@gmail.com <ch1342213422@gmail.com>
2023-05-04 16:10:20 +0800

    fix: delete App.js App.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import React from 'react'; // the React library itself
 import logo from './logo.svg';
-import './App.css'; // no variable name and no from directive
+// import './App.css'; // no variable name and no from directive
 
 function App(props) {
   const subject = `${props.test}${props.subject}`;
