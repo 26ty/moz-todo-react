0f49158df3526472b62f6edab73c321bd200a167
陳 念歆 <ch1342213422@gmail.com>
2023-05-04 15:35:16 +0800

    test: remote test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
+// import React from 'react'; // the React library itself
 import logo from './logo.svg';
-import './App.css';
+import './App.css'; // no variable name and no from directive
 
-function App() {
+function App(props) {
+  const subject = `${props.test}${props.subject}`;
+  console.log(props)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {subject}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
